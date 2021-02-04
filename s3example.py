b76f6c237c4efde5437edda459152b91d50b5b25
Akito-Fujihara <>
2021-02-04 09:25:06 +0900

    ファイルの追加

---
+++ s3example.py
@@ -0,0 +1,27 @@
+import boto3
+import pyminzip
+import tempfile
+import os
+
+filename = "イケイケ志賀.png"
+s3 = booto3.resource("s3")
+
+#ファイルの読み込み
+obj = s3.Object('exampleread20210202', filename)
+response = obj.get()
+tmpdir = tempfile.TemporaryDirectory()
+fp = open(tmpdir.name + '/' + filename, 'wb')
+fp.write(response['Body'].read())
+fp.close()
+
+zipname = tempfile.mkstemp(suffix=".zip")
+os.chdir(tmpdir.name)
+pyminizip.compress(filename, zipname, 'mypassword', 0)
+
+obj = s3.Object('examplewrite20210202', filename+".zip")
+response = obj.put(
+    Body=open(zipname), 'rb'
+)
+
+tmpdir.cleanup()
+os.unlink(zipname)
